d0a7faccaa9ae10c6aba6fe8c53a29f80dbae06e
Hartaithan <hartaithan@gmail.com>
2023-12-02 23:32:28 +0900

    read payload

--- fill.py
+++ fill.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+# import pyautogui
+
+# START
+print('Script started')
+
+# FOCUS MUSIC APP
+
+# PLAY
+
+# READ PAYLOAD
+with open('payload.txt', 'r', encoding='utf-8') as file:
+    # LOOP THROUGH PAYLOAD
+    for line in file:
+        print(line.strip())
+        # FOR EACH ITEM REPEAT N TIMES MOUSE CLICK ON LAST SECOND OF THE TIMELINE
+        # pyautogui.moveTo(1343,915)
+
+# DONE
+print('Script finished')
