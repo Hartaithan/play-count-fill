7292e49fed4fd39f4adcf528da7a0e7049a1212d
Hartaithan <hartaithan@gmail.com>
2023-12-03 17:31:28 +0900

    loop through items and increase counts

--- fill.py
+++ fill.py
@@ -1,13 +1,11 @@
 import os
-# import pyautogui
+import time
 
 # START
 print('Script started')
 
 # FOCUS MUSIC APP
 os.system('''/usr/bin/osascript -e 'tell application "Music" to activate' ''')
-os.system(
-    '''/usr/bin/osascript -e 'tell application "System Events" to keystroke "f" using {control down, command down}' ''')
 
 # PLAY
 os.system('''/usr/bin/osascript -e 'tell application "Music" to play' ''')
@@ -15,10 +13,25 @@ os.system('''/usr/bin/osascript -e 'tell application "Music" to play' ''')
 # READ PAYLOAD
 with open('payload.txt', 'r', encoding='utf-8') as file:
     # LOOP THROUGH PAYLOAD
-    for line in file:
-        print(line.strip())
-        # FOR EACH ITEM REPEAT N TIMES MOUSE CLICK ON LAST SECOND OF THE TIMELINE
-        # pyautogui.moveTo(1343,915)
+    for i, line in enumerate(file):
+        # FOR EACH ITEM GET PLAY COUNT
+        track = line.strip()
+        count = int(track)
+        print('Current track: ', i, ', Play count: ', count)
+        # PLAY TRACK N TIMES
+        for i in range(count):
+            os.system(
+                '''/usr/bin/osascript -e 'tell application "Music" to set player position to (get duration of current track) - 0.5' ''')
+            print('Count is set ', i + 1, ' times')
+            time.sleep(1)
+        # SKIP TO NEXT TRACK
+        os.system(
+            '''/usr/bin/osascript -e 'tell application "Music" to set song repeat to off' ''')
+        os.system(
+            '''/usr/bin/osascript -e 'tell application "Music" to next track' ''')
+        os.system(
+            '''/usr/bin/osascript -e 'tell application "Music" to set song repeat to one' ''')
+        time.sleep(1)
 
 # DONE
 print('Script finished')
